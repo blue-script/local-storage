291f516c0a0e2e72baf78b344cef672189911a3a
demianov-an <alexm92@inbox.ru>
2024-02-19 16:06:32 +0600

    07 - Использование localstorage в Redux, part 2 (PreloadedState)

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -1,10 +1,16 @@
-import {combineReducers, legacy_createStore} from "redux"
+import {applyMiddleware, combineReducers, legacy_createStore} from "redux"
 import {counterReducer} from "./counter-reducer"
+import {thunk} from "redux-thunk"
+import {loadState, saveState} from "../utils/localStorage-utils"
 
 const rootReducer = combineReducers({
   counter: counterReducer
 })
 
-export const store = legacy_createStore(rootReducer)
-export type AppStoreType = typeof store
+export const store = legacy_createStore(rootReducer, loadState(), applyMiddleware(thunk))
+store.subscribe(() => {
+  saveState(store.getState());
+})
+
+// export type AppStoreType = typeof store
 export type AppStateType = ReturnType<typeof rootReducer>
